9500e95c43f37a44dd8e51257b72df2c096c34b9
= <aldhiego666@gmail.com>
2023-06-17 19:01:38 -0300

    Adição de estado na SearchBar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,14 +1,19 @@
 import { BsSearch } from 'react-icons/bs';
 import './style.css';
+import { useState } from 'react';
 
 export const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
       <input 
         type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
-        required 
+        onChange={({target}) => setSearchValue(target.value)}
+        required
       />
 
       <button type="submit" className="search__button">
