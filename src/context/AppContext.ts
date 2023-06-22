8d769e6147e2c47d8a29787419ddb267c82b9ac4
= <aldhiego666@gmail.com>
2023-06-21 21:50:44 -0300

    Formatando código

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -1,9 +1,11 @@
 import { createContext } from 'react';
 import { TProduct } from '../services/api/fetchProducts';
+
 type TAppContext = {
   products: TProduct[];
   setProducts: (ListProduct: TProduct[]) => void;
   loading: boolean;
   setLoading: (isLoading: boolean) => void;
 }
+
 export const AppContext = createContext({} as TAppContext);
