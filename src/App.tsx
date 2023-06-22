ba451bb254bef89b148846fd3e2ddde58ef6f45a
= <aldhiego666@gmail.com>
2023-06-21 21:47:51 -0300

    Envolvendo o provedor de contexo no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Header } from './components/Header';
 import { Products } from './components/Products';
+import { AppContextProvider } from './context/AppContextProvider';
 
 export const App = () => {
 
   return (
-    <>
+    <AppContextProvider>
       <Header/>
       <Products/>
     </>
