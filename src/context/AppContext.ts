dd5601e4bc8deb28447ecd9f7216090c467309dd
= <aldhiego666@gmail.com>
2023-06-21 23:39:40 -0300

    Adicionando atributo e método no type do contexto

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -4,8 +4,12 @@ import { TProduct } from '../services/api/fetchProducts';
 type TAppContext = {
   products: TProduct[];
   setProducts: (ListProduct: TProduct[]) => void;
+
   loading: boolean;
   setLoading: (isLoading: boolean) => void;
+
+  cartItem: TProduct[];
+  setCartItems: (product: TProduct[]) => void;
 }
 
 export const AppContext = createContext({} as TAppContext);
