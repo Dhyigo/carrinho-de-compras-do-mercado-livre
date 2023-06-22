8ac1fc2460146667358ec4b125c62057cae81cee
= <aldhiego666@gmail.com>
2023-06-21 22:11:34 -0300

    Consumo do contexto no componente Products

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from 'react';
-import './style.css';
-import { TProduct, fetchProducts } from '../../services/api/fetchProducts';
+import { memo, useContext, useEffect } from 'react';
+
+import { fetchProducts } from '../../services/api/fetchProducts';
 import { ProductCard } from '../ProductCard';
 import { Loading } from '../Loading';
+import { AppContext } from '../../context/AppContext';
+
+import './style.css';
 
 export const Products = () => {
-  const [products, setProducts] = useState([] as TProduct[]);
-  const [loading, setLoading] = useState(false);
-
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
+  
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
 
       const products = await fetchProducts('iPhone');
+      
       setProducts(products);
       setLoading(false);
     };
@@ -28,6 +31,5 @@ export const Products = () => {
         }
       </section>
     )
-    
   );
 };
