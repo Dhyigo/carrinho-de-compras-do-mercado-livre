7824e9feaffe67f3a62e672ad91d03f0721dac0d
= <aldhiego666@gmail.com>
2023-06-22 20:58:19 -0300

    Soma dos items do carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,16 +2,20 @@ import { useContext } from 'react';
 import { CartItem } from '../CartItem';
 import { AppContext } from '../../context/AppContext';
 import './style.css';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 export const Cart = () =>  {
   const { cartItems } = useContext(AppContext);
+
+  const totalPrice = cartItems.reduce((acc, { price }) => acc + price, 0);
+  
   return (
     <section className="cart">
       <div className="cart-items">
         {cartItems.map((cartItem) => (<CartItem key={cartItem.id} data={cartItem} />))}
       </div>
 
-      <div className="cart-resume">resumo</div>
+      <div className="cart-resume">{formatCurrency(totalPrice)}</div>
     </section>
   );
 };
