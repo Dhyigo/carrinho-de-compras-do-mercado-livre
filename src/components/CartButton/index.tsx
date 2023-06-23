0d19cd50ae1062a49c825bf7128f498ac42c1f60
= <aldhiego666@gmail.com>
2023-06-22 21:07:11 -0300

    Feedback de produtos no carrinho

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,11 +1,15 @@
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './style.css';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 
 export const CartButton = () => {
+  const { cartItems } = useContext(AppContext);
+
   return (
     <button type="button" className="cart__button">
       <AiOutlineShoppingCart />
-      <span className="cart-status">1</span>
+      {cartItems.length > 0 && <span className="cart-status">{cartItems.length}</span>}
     </button>
   );
 };
